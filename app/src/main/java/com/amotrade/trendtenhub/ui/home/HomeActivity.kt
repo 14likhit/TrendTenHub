471c52a1a50451d1f636633e66c7fe9c7af06850
14likhit <jaijalaram@4495BCU>
2021-06-28 13:10:10 +0530

    - Refactored Activity --> Renamed from MainActivity.kt to HomeActivity.kt

--- app/src/main/java/com/amotrade/trendtenhub/ui/home/HomeActivity.kt
+++ app/src/main/java/com/amotrade/trendtenhub/ui/home/HomeActivity.kt
@@ -1,11 +1,12 @@
-package com.amotrade.trendtenhub
+package com.amotrade.trendtenhub.ui.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.amotrade.trendtenhub.R
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home)
     }
 }
